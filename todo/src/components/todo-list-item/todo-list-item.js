aefdb70f09cef1145c406d0d81e9929c2c1eff31
StopTouchingMeElmo <mezhdumirie@gmail.com>
2020-07-20 03:16:01 +0300

    class comp done

--- todo/src/components/todo-list-item/todo-list-item.js
+++ todo/src/components/todo-list-item/todo-list-item.js
@@ -3,6 +3,19 @@ import React, { Component } from "react";
 import "./todo-list-item.css";
 
 export default class ToDoListItem extends Component {
+  // Классический способ передачи функции внутрь самого объекта.
+  /* constructor() {
+    super();
+    this.onContentClick = () => {
+      console.log(`Done: ${this.props.content}`);
+    };
+  } */
+
+  // Способ proposal Class Fields, еще не вошедший в стандарт.
+  onContentClick = () => {
+    console.log(`Done: ${this.props.content}`);
+  };
+
   render() {
     const { content, important = false } = this.props;
     const spanStyle = {
@@ -12,7 +25,11 @@ export default class ToDoListItem extends Component {
 
     return (
       <span className="todo-list-item">
-        <span style={spanStyle} className="todo-list-item-content">
+        <span
+          style={spanStyle}
+          className="todo-list-item-content"
+          onClick={this.onContentClick}
+        >
           {content}
         </span>
 
